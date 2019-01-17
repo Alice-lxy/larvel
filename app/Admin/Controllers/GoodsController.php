f2d1b3af41fe1145f35d29e621a06a31526fd829
lxy <1325835938@qq.com>
2019-01-17 19:10:50 +0800

    goods/user

---
+++ app/Admin/Controllers/GoodsController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Model\Goods;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class GoodsController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Goods);
+
+        $grid->model()->orderBy('goods_id','desc');     //倒序排序
+
+        $grid->goods_id('Goods id');
+        $grid->goods_name('Goods name');
+        $grid->store('Store');
+        $grid->cart_id('Cart id');
+        $grid->price('Price');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+        $grid->paginate(5);
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Goods::findOrFail($id));
+
+        $show->goods_id('Goods id');
+        $show->goods_name('Goods name');
+        $show->store('Store');
+        $show->cart_id('Cart id');
+        $show->price('Price');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Goods);
+        $form->text('goods_name', 'Goods name');
+        $form->number('store', 'Store');
+        $form->number('cart_id', 'Cart id');
+        $form->currency('price', 'Price');
+        return $form;
+    }
+}
