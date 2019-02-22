9f9ca4ae88226f66c85f5bc0b76607855763ed40
root <root@localhost.localdomain>
2019-02-22 10:15:11 +0800

    永久素材4

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -179,10 +179,15 @@ class WeixinMediaController extends Controller
         ]);
 
         $body = $response->getBody();
-        echo $body;echo '<hr>';
+        //echo $body;echo '<hr>';
         $d = json_decode($body,true);
-        echo '<pre>';print_r($d);echo '</pre>';
-
+       // echo '<pre>';print_r($d);echo '</pre>';
+        if($d['errcode'] == 0){
+            echo "success";
+        }else{
+            echo "fail，请重试";echo '</br>';
+            echo $d['errmsg'];
+        }
 
     }
 
@@ -194,21 +199,21 @@ class WeixinMediaController extends Controller
 
     public function formTest(Request $request)
     {
-        echo '<pre>';print_r($_POST);echo '</pre>';echo '<hr>';
-        echo '<pre>';print_r($_FILES);echo '</pre>';echo '<hr>';
+//        echo '<pre>';print_r($_POST);echo '</pre>';echo '<hr>';
+//        echo '<pre>';print_r($_FILES);echo '</pre>';echo '<hr>';
         //exit();
         //保存文件
         $img_file = $request->file('media');
-        echo '<pre>';print_r($img_file);echo '</pre>';echo '<hr>';
+       // echo '<pre>';print_r($img_file);echo '</pre>';echo '<hr>';
 
         $img_origin_name = $img_file->getClientOriginalName();//图片原始名称
-        echo 'originName: '.$img_origin_name;echo '</br>';
+       // echo 'originName: '.$img_origin_name;echo '</br>';
         $file_ext = $img_file->getClientOriginalExtension(); //获取文件扩展名
-        echo 'ext: '.$file_ext;echo '</br>';
+        //echo 'ext: '.$file_ext;echo '</br>';
 
         //重命名
         $new_file_name = str_random(15). '.'.$file_ext;
-        echo 'new_file_name: '.$new_file_name;echo '</br>';
+      //  echo 'new_file_name: '.$new_file_name;echo '</br>';
 
         //文件保存路
 
@@ -216,7 +221,7 @@ class WeixinMediaController extends Controller
         //保存文件
         $save_file_path = $request->media->storeAs('form_test',$new_file_name);       //返回保存成功之后的文件路径
 
-        echo 'save_file_path: '.$save_file_path;echo '<hr>';
+       // echo 'save_file_path: '.$save_file_path;echo '<hr>';
 
         //上传至微信永久素材
         $this->upMaterialTest($save_file_path);
