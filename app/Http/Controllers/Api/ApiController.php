cacc0c704dcec545d8c7ab103b78fdb81580e116
Alice-lxy <1325835938@qq.com>
2019-03-21 18:59:12 +0800

    debug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -88,7 +88,7 @@ class ApiController extends Controller
     }
 
     public function login(){
-       echo json_encode($_POST);die;
+       //echo json_encode($_POST);die;
         $name = $_POST['name'];
         $pwd = md5($_POST['pwd']);
         //echo $pwd;die;
