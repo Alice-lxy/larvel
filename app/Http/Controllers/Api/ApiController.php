7e7b295f1ebe0b7293fdd109b457cc68342b0f7d
Alice-lxy <1325835938@qq.com>
2019-03-23 10:42:21 +0800

    debug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -110,7 +110,8 @@ class ApiController extends Controller
 
         $res = curl_exec($ch);
 
-         var_dump(json_encode($res));
+         $data=json_encode($res);
+         var_dump(json_decode($data));
        // print_r($res);
 
 
